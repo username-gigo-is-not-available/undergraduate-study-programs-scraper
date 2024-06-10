ad58e83fac3e6da44d3ff233cade6f213d783fdc
unknown <grigorij.josifovski@gmail.com>
2024-06-10 18:04:49 +0200

    Add clean_whitespace and validate_course decorators, modify parse_course_semester and parse_course_season

--- extract/course_details.py
+++ extract/course_details.py
@@ -4,74 +4,62 @@ from bs4 import BeautifulSoup, Tag
 from constants import (
     COURSE_DETAILS_PREREQUISITE_SELECTOR,
     COURSE_DETAILS_PROFESSORS_SELECTOR,
-    COURSE_DETAILS_YEAR_SELECTOR,
+    COURSE_DETAILS_SEMESTER_NUMBER_SELECTOR,
     COURSE_DETAILS_CODE_SELECTOR,
     COURSE_DETAILS_NAME_SELECTOR,
-    COURSE_TABLES_CLASS_NAME
+    COURSE_TABLES_CLASS_NAME, COURSE_DETAILS_SEMESTER_SEASON_SELECTOR
 )
-from handlers import handle_invalid_course
+from decorators import clean_whitespace, validate_course
+from enums import CourseSeason
 from models import CourseDetails
-from extract.utils import parse_object
+from parsers import parse_object
 
 
-def parse_course_table(soup: BeautifulSoup) -> Tag:
+def get_course_table(soup: BeautifulSoup) -> Tag:
     return soup.select_one(COURSE_TABLES_CLASS_NAME)
 
 
-def parse_course_year(course_table: Tag) -> int:
-    return int(course_table.select_one(COURSE_DETAILS_YEAR_SELECTOR).text.strip())
-
-
+@clean_whitespace
 def parse_course_professors(course_table: Tag) -> str:
-    professors = course_table.select_one(COURSE_DETAILS_PROFESSORS_SELECTOR).text.strip().replace("\n", ",")
-    return professors if professors else "нема"
+    return course_table.select_one(COURSE_DETAILS_PROFESSORS_SELECTOR).text
 
 
+@clean_whitespace
 def parse_course_prerequisite(course_table: Tag) -> str:
-    prerequisite = course_table.select_one(COURSE_DETAILS_PREREQUISITE_SELECTOR).text.strip()
-    return prerequisite if prerequisite else "нема"
+    return course_table.select_one(COURSE_DETAILS_PREREQUISITE_SELECTOR).text
 
 
+@clean_whitespace
 def parse_course_code(course_table: Tag) -> str:
-    return course_table.select_one(COURSE_DETAILS_CODE_SELECTOR).text.strip()
+    return course_table.select_one(COURSE_DETAILS_CODE_SELECTOR).text
 
 
+@clean_whitespace
 def parse_course_name(course_table: Tag) -> str:
-    return course_table.select_one(COURSE_DETAILS_NAME_SELECTOR).text.strip()
+    return course_table.select_one(COURSE_DETAILS_NAME_SELECTOR).text
 
 
-def parse_course_semester(course_season: str, course_year: int) -> int:
-    semester = course_year * 2
-    return semester if course_season == 'SUMMER' else semester - 1
+def parse_course_semester(course_table: Tag) -> int:
+    return int(course_table.select_one(COURSE_DETAILS_SEMESTER_NUMBER_SELECTOR).text.strip())
 
 
-def parse_course_season(course_code: str) -> str:
-    return 'WINTER' if course_code[3] == 'W' else 'SUMMER' if course_code[3] == 'L' else 'WINTER'
+@clean_whitespace
+def parse_course_season(course_table: Tag) -> str:
+    return CourseSeason(course_table.select_one(COURSE_DETAILS_SEMESTER_SEASON_SELECTOR).text.capitalize()).value
 
 
-def parse_course_level(course_code: str) -> int:
-    return int(course_code[4])
-
-
-parse_fields_course_table = {
+parse_fields = {
     'code': parse_course_code,
     'name': parse_course_name,
-    'year': parse_course_year,
+    'semester': parse_course_semester,
+    'season': parse_course_season,
     'professors': parse_course_professors,
     'prerequisite': parse_course_prerequisite
 }
 
-parse_fields_course_code = {
-    'level': parse_course_level,
-    'season': parse_course_season,
-}
 
-
+@validate_course
 def parse_course_details(course_table: Tag) -> CourseDetails:
-    fields = parse_object(parse_fields_course_table, course_table)
-    code = fields.get('code')
-    fields.update(parse_object(parse_fields_course_code, code))
-    fields.update({'semester': parse_course_semester(fields.get('season'), fields.get('year'))})
-    course_details = handle_invalid_course(CourseDetails(**fields))
+    course_details = CourseDetails(**parse_object(parse_fields, course_table))
     logging.info(f"Scraped course details {course_details}")
     return course_details
