0f26ebeecb16d2f425faeb84470ecfd3962fcd19
unknown <grigorij.josifovski@gmail.com>
2024-06-10 12:02:23 +0200

    Add validate url and clean_whitespace decorator

--- extract/study_program.py
+++ extract/study_program.py
@@ -5,20 +5,23 @@ from constants import (
     STUDY_PROGRAM_DURATION_SELECTOR,
     STUDY_PROGRAM_URL_SELECTOR,
 )
+from decorators import clean_whitespace, process_url
+from parsers import parse_object
 from models import StudyProgram
-from extract.utils import prepend_base_url
 
 
+@clean_whitespace
 def parse_study_program_name(element: Tag) -> str:
-    return element.select_one(STUDY_PROGRAM_NAME_SELECTOR).text.strip()
+    return element.select_one(STUDY_PROGRAM_NAME_SELECTOR).text
 
 
 def parse_study_program_duration(element: Tag) -> int:
     return int(element.select_one(STUDY_PROGRAM_DURATION_SELECTOR).text.strip())
 
 
+@process_url
 def parse_study_program_url(element: Tag) -> str:
-    return prepend_base_url(element.select_one(STUDY_PROGRAM_URL_SELECTOR)['href'])
+    return element.select_one(STUDY_PROGRAM_URL_SELECTOR)['href']
 
 
 parse_fields = {
@@ -30,5 +33,5 @@ parse_fields = {
 
 def parse_study_program(element: Tag) -> StudyProgram:
     return StudyProgram(
-        **{field: parse_fields[field](element) for field in parse_fields}
+        **parse_object(parse_fields, element)
     )
