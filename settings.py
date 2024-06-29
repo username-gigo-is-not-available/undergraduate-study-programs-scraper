2a3568d738d8c156786cdb64753f6702380c62d0
unknown <grigorij.josifovski@gmail.com>
2024-06-29 17:09:28 +0200

    Extract static variables to file, add types, introduce THREADS_PER_CPU_CORE variable

--- settings.py
+++ settings.py
@@ -1,29 +1,26 @@
+import os
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 
 from dotenv import dotenv_values
 
+from static import THREADS_PER_CPU_CORE
 
-BASE_URL: str = "https://finki.ukim.mk"
-STUDY_PROGRAMS_URL: str = "https://finki.ukim.mk/mk/dodiplomski-studii"
-
-INVALID_COURSE_CODES: list[str] = ['F23L1S026', 'F23L2S066']
-
-ENVIRONMENT_VARIABLES = dotenv_values(".env")
+ENVIRONMENT_VARIABLES: dict[str, str | None] = dotenv_values(".env")
 
 for variable_name, variable_value in ENVIRONMENT_VARIABLES.items():
     if not variable_value:
         raise RuntimeError(f"{variable_name} is not set!")
 
 
-def get_max_workers():
-    return None if ENVIRONMENT_VARIABLES['MAX_WORKERS'] == 'MAX_WORKERS' else int(ENVIRONMENT_VARIABLES['MAX_WORKERS'])
+def get_max_workers() -> int:
+    return THREADS_PER_CPU_CORE * os.cpu_count() if ENVIRONMENT_VARIABLES.get('MAX_WORKERS') == 'MAX_WORKERS' else int(ENVIRONMENT_VARIABLES['MAX_WORKERS'])
 
 
-def get_executor():
+def get_executor() -> ThreadPoolExecutor | ProcessPoolExecutor:
     max_workers = get_max_workers()
-    if ENVIRONMENT_VARIABLES['EXECUTOR_TYPE'] == 'THREAD':
+    if ENVIRONMENT_VARIABLES.get('EXECUTOR_TYPE') == 'THREAD':
         return ThreadPoolExecutor(max_workers=max_workers)
-    elif ENVIRONMENT_VARIABLES['EXECUTOR_TYPE'] == 'PROCESS':
+    elif ENVIRONMENT_VARIABLES.get('EXECUTOR_TYPE') == 'PROCESS':
         return ProcessPoolExecutor(max_workers=max_workers)
     else:
-        raise ValueError("Invalid executor type")
+        raise ValueError('Invalid executor type')
