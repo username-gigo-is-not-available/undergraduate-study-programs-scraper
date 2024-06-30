ffea4d1602c4efc8bf9dbd78a6f1accd64e1f3cb
unknown <grigorij.josifovski@gmail.com>
2024-06-30 20:03:01 +0200

    Remove INVALID_COURSE_CODES

--- static.py
+++ static.py
@@ -1,4 +1,3 @@
 BASE_URL: str = "https://finki.ukim.mk"
 STUDY_PROGRAMS_URL: str = "https://finki.ukim.mk/mk/dodiplomski-studii"
-INVALID_COURSE_CODES: list[str] = ['F23L1S026', 'F23L2S066']
 THREADS_PER_CPU_CORE: int = 5
