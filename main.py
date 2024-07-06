08af1e4f24be45554cdf4de455766e8c6083a199
unknown <grigorij.josifovski@gmail.com>
2024-07-06 16:31:47 +0200

    Add types

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import asyncio
 import time
 import logging
+from asyncio import AbstractEventLoop
+from concurrent.futures import Executor
 
+from data_models.course_details.model import CourseDetails
+from data_models.curriculum.model import Curriculum
+from data_models.study_program.model import StudyProgram
 from utils.asynchronisity import get_study_programs_data, get_course_data, \
     get_curriculum_data, save_data
 from settings import ENVIRONMENT_VARIABLES, get_executor
@@ -12,15 +17,15 @@ logging.basicConfig(level=logging.INFO)
 
 async def main():
     logging.info("Starting...")
-    start = time.perf_counter()
-    loop = asyncio.get_event_loop()
-    executor_type = get_executor()
-    output_directory = ENVIRONMENT_VARIABLES.get('OUTPUT_DIRECTORY_PATH', '.')
-    study_programs_data = await get_study_programs_data()
+    start: float = time.perf_counter()
+    loop: AbstractEventLoop = asyncio.get_event_loop()
+    executor_type: Executor = get_executor()
+    output_directory: str = ENVIRONMENT_VARIABLES.get('OUTPUT_DIRECTORY_PATH', '.')
+    study_programs_data: list[StudyProgram] = await get_study_programs_data()
     with executor_type as executor:
-        curricula_data = await get_curriculum_data(executor, loop, study_programs_data)
-        courses_data = await get_course_data(executor, loop, curricula_data)
-        data = prepare_data_for_saving(study_programs_data, curricula_data, courses_data)
+        curricula_data: list[Curriculum] = await get_curriculum_data(executor, loop, study_programs_data)
+        courses_data: list[CourseDetails] = await get_course_data(executor, loop, curricula_data)
+        data: list[dict[str, str | tuple]] = prepare_data_for_saving(study_programs_data, curricula_data, courses_data)
         await save_data(executor, loop, data, output_directory)
 
     logging.info(f"Time taken: {time.perf_counter() - start}")
