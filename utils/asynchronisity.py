274cdda7d143a776d16afdb669f44fe6da930a32
unknown <grigorij.josifovski@gmail.com>
2024-07-06 16:32:46 +0200

    Add types, extract get_data_chunk_size() and siplit_data_into_chunks to concurrency.py

--- utils/asynchronisity.py
+++ utils/asynchronisity.py
@@ -1,7 +1,7 @@
 import asyncio
 import csv
 import logging
-from asyncio import AbstractEventLoop
+from asyncio import AbstractEventLoop, Future
 from concurrent.futures import Executor
 from pathlib import Path
 
@@ -14,8 +14,7 @@ from data_models.curriculum.model import Curriculum
 from data_models.curriculum.parse import parse_curriculum_data
 from data_models.study_program.model import StudyProgram
 from data_models.study_program.parse import parse_study_program_data
-from settings import get_max_workers
-from utils.concurrency import get_unique_course_headers
+from utils.concurrency import get_unique_course_headers, split_data_into_chunks
 from utils.filtering import is_macedonian_study_program
 
 
@@ -29,8 +28,8 @@ def run_parse_curriculum_data(study_program: StudyProgram) -> list[Curriculum]:
 
 
 async def get_curriculum_data(executor: Executor, loop: AbstractEventLoop, study_programs_data: list[StudyProgram]) -> list[Curriculum]:
-    tasks = [loop.run_in_executor(executor, run_parse_curriculum_data, study_program) for study_program in
-             study_programs_data]
+    tasks: list[Future[list[Curriculum]]] = [loop.run_in_executor(executor, run_parse_curriculum_data, study_program) for study_program in
+                                             study_programs_data]
     return [curriculum for sublist in await asyncio.gather(*tasks) for curriculum in sublist]
 
 
@@ -39,28 +38,28 @@ def run_parse_course_details_data(course_headers: list[CourseHeader]) -> list[Co
 
 
 async def get_course_data(executor: Executor, loop: AbstractEventLoop, curriculum_data: list[Curriculum]) -> list[CourseDetails]:
-    course_headers = get_unique_course_headers(curriculum_data)
-    chunk_size = len(course_headers) // get_max_workers()
-    chunks = [course_headers[i:i + chunk_size] for i in
-              range(0, len(course_headers), chunk_size)]
-    tasks = [loop.run_in_executor(executor, run_parse_course_details_data, course_headers_chunk) for course_headers_chunk
-             in chunks]
+    course_headers: list[CourseHeader] = get_unique_course_headers(curriculum_data)
+    chunks: list[list[CourseHeader]] = split_data_into_chunks(course_headers)
+    tasks: list[Future[list[CourseDetails]]] = [loop.run_in_executor(executor, run_parse_course_details_data, course_headers_chunk) for
+                                                course_headers_chunk in chunks]
     return [course for sublist in await asyncio.gather(*tasks) for course in sublist]
 
 
-def run_save_data_to_file(data: list[dict[str, str | tuple]], output_dir: str) -> None:
+def run_save_data_to_file(data: dict[str, str | tuple], output_dir: str) -> None:
     return asyncio.run(save_data_to_file(data, output_dir))
 
 
 async def save_data(executor: Executor, loop: AbstractEventLoop, data: list[dict[str, str | tuple]], output_dir: str) -> None:
     Path(output_dir).mkdir(parents=True, exist_ok=True)
-    tasks = [loop.run_in_executor(executor, run_save_data_to_file, item, output_dir) for item in data]
+    tasks: list[Future[None]] = [loop.run_in_executor(executor, run_save_data_to_file, item, output_dir) for item in data]
     await asyncio.gather(*tasks)
 
 
 async def save_data_to_file(data_dict: dict[str, str | tuple], output_dir: str) -> None:
-    file_name, rows, fields = data_dict.get("file_name"), data_dict.get("rows"), data_dict.get("fields")
-    logging.info(f"Saving data to file: {file_name}")
+    file_name: str = data_dict.get("file_name")
+    rows: tuple[str | int] = data_dict.get("rows")
+    fields: tuple[str] = data_dict.get("fields")
+    logging.info(f"Saving data to file: {file_name}.csv")
     with open(f"{Path(output_dir)}/{file_name}.csv", "w", newline="", encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(fields)
--- utils/concurrency.py
+++ utils/concurrency.py
@@ -1,9 +1,12 @@
 from threading import Lock
 
+from data_models.course_details.model import CourseDetails
 from data_models.course_header.model import CourseHeader
 from data_models.curriculum.model import Curriculum
+from data_models.study_program.model import StudyProgram
+from settings import get_max_workers
 
-lock = Lock()
+lock: Lock = Lock()
 
 
 def get_unique_course_headers(curriculums: list[Curriculum]) -> list[CourseHeader]:
@@ -26,3 +29,12 @@ def get_unique_course_headers(curriculums: list[Curriculum]) -> list[CourseHeade
         finally:
             lock.release()
     return list(result.values())
+
+
+def get_data_chunk_size(data: list) -> int:
+    return len(data) // get_max_workers()
+
+
+def split_data_into_chunks(data: list) -> list[list[CourseHeader | Curriculum | CourseDetails | StudyProgram]]:
+    chunk_size: int = get_data_chunk_size(data)
+    return [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
