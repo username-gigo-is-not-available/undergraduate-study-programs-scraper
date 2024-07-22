e951c34b58239388203924ca92a355b7127c1d04
unknown <grigorij.josifovski@gmail.com>
2024-07-22 20:50:34 +0200

    Add typing

--- static.py
+++ static.py
@@ -2,7 +2,7 @@ import os
 
 from dotenv import dotenv_values
 
-ENVIRONMENT_VARIABLES: dict = dotenv_values('.env')
+ENVIRONMENT_VARIABLES: dict[str, str] = dotenv_values('.env')
 
 BASE_URL: str = "https://finki.ukim.mk"
 STUDY_PROGRAMS_URL: str = "https://finki.ukim.mk/mk/dodiplomski-studii"
