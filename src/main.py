9d06829e052ffa93fb55e0b522e76a6d7168df22
unknown <grigorij.josifovski@gmail.com>
2024-09-29 15:49:06 +0200

    Refactor to async queue based multithreaded approach

---
+++ src/main.py
@@ -0,0 +1,30 @@
+import asyncio
+import time
+import logging
+from concurrent.futures import Executor
+
+from src.parsers.curriculum_parser import CurriculumParser
+from src.parsers.course_details_parser import CourseDetailsParser
+from src.parsers.study_program_parser import StudyProgramParser
+from src.decorators import check_if_url_is_accessible
+from settings import get_executor
+
+logging.basicConfig(level=logging.INFO)
+
+
+@check_if_url_is_accessible
+async def main():
+    logging.info("Starting...")
+    start: float = time.perf_counter()
+    executor_type: Executor = get_executor()
+    await StudyProgramParser.scrape_and_save_data()
+    tasks = []
+    with executor_type as executor:
+        tasks.append(asyncio.create_task(CurriculumParser.scrape_and_save_data(executor=executor)))
+        tasks.append(asyncio.create_task(CourseDetailsParser.scrape_and_save_data(executor=executor)))
+        await asyncio.gather(*tasks)
+        logging.info(f"Time taken: {time.perf_counter() - start:.2f} seconds")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
