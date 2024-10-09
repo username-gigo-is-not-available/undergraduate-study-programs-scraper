82722fe4cfec268d7843c0f2ea392dba846ce987
unknown <grigorij.josifovski@gmail.com>
2024-10-09 19:15:17 +0200

    Fix requirements.txt add MinIO support

--- src/mixins.py
+++ src/mixins.py
@@ -6,13 +6,16 @@ import ssl
 import threading
 from asyncio import AbstractEventLoop
 from concurrent.futures import Executor
+from io import StringIO, BytesIO
 from pathlib import Path
-from typing import NamedTuple
+from typing import NamedTuple, BinaryIO
 
 import aiohttp
 import certifi
 from aiohttp import ClientTimeout
 from bs4 import BeautifulSoup
+from minio import Minio
+from minio.error import S3Error
 
 from static import ENVIRONMENT_VARIABLES
 
@@ -34,7 +37,13 @@ class ThreadSafetyMixin:
 
 
 class StorageMixin:
+    STORAGE_TYPE: str = ENVIRONMENT_VARIABLES.get('STORAGE_TYPE')
     OUTPUT_DIRECTORY_PATH: Path = Path(ENVIRONMENT_VARIABLES.get('OUTPUT_DIRECTORY_PATH'))
+    MINIO_ENDPOINT_URL: str = ENVIRONMENT_VARIABLES.get('MINIO_ENDPOINT_URL')
+    MINIO_ACCESS_KEY: str = ENVIRONMENT_VARIABLES.get('MINIO_ACCESS_KEY')
+    MINIO_SECRET_KEY: str = ENVIRONMENT_VARIABLES.get('MINIO_SECRET_KEY')
+    MINIO_BUCKET_NAME: str = ENVIRONMENT_VARIABLES.get('MINIO_BUCKET_NAME')
+    MINIO_CLIENT = Minio(MINIO_ENDPOINT_URL, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET_KEY, secure=False)
 
     @classmethod
     def save_data_to_file(cls, data: list[NamedTuple], field_names: list[str], file_name: Path) -> None:
@@ -47,11 +56,50 @@ class StorageMixin:
             logging.error(f"Failed to save data to file {file_name}: {e}")
 
     @classmethod
+    def save_data_to_minio(cls, data: list[NamedTuple], field_names: list[str], file_name: Path) -> None:
+
+        try:
+
+            string_buffer: StringIO = StringIO()
+            writer = csv.writer(string_buffer)
+            writer.writerow(field_names)
+            writer.writerows(data)
+
+            bytes_buffer: BinaryIO = BytesIO()
+            bytes_buffer.write(string_buffer.getvalue().encode('utf-8'))
+            data_length = bytes_buffer.tell()
+            bytes_buffer.seek(0)
+
+            logging.info(f"Saving data to MinIO bucket {cls.MINIO_BUCKET_NAME} as {file_name}")
+
+            cls.MINIO_CLIENT.put_object(
+                bucket_name=cls.MINIO_BUCKET_NAME,
+                object_name=str(file_name),
+                data=bytes_buffer,
+                length=data_length,
+                content_type='text/csv'
+            )
+
+        except S3Error as e:
+            logging.error(f"Failed to save data to MinIO bucket {cls.MINIO_BUCKET_NAME}: {e}")
+
+    @classmethod
     async def save_data(cls, data: list[NamedTuple], file_name: Path, executor: Executor = None) -> None:
         loop: AbstractEventLoop = asyncio.get_event_loop()
-        cls.OUTPUT_DIRECTORY_PATH.mkdir(parents=True, exist_ok=True)
-        logging.info(f"Saving data to file {file_name}")
-        await loop.run_in_executor(executor, cls.save_data_to_file, data, list(data)[0]._fields, file_name)
+
+        if cls.STORAGE_TYPE == 'FILE':
+
+            cls.OUTPUT_DIRECTORY_PATH.mkdir(parents=True, exist_ok=True)
+            logging.info(f"Saving data to file {file_name}")
+            await loop.run_in_executor(executor, cls.save_data_to_file, data, list(data)[0]._fields, file_name)
+
+        elif cls.STORAGE_TYPE == 'MINIO':
+
+            if not cls.MINIO_CLIENT.bucket_exists(cls.MINIO_BUCKET_NAME):
+                cls.MINIO_CLIENT.make_bucket(cls.MINIO_BUCKET_NAME)
+                logging.info(f"Created MinIO bucket {cls.MINIO_BUCKET_NAME}")
+            logging.info(f"Saving data to MinIO bucket {cls.MINIO_BUCKET_NAME}")
+            await loop.run_in_executor(executor, cls.save_data_to_minio, data, list(data)[0]._fields, file_name)
 
 
 class HTTPClientMixin:
--- src/static.py
+++ src/static.py
@@ -1,15 +1,12 @@
 import os
-from pathlib import Path
 
 from dotenv import dotenv_values
 
-ENVIRONMENT_VARIABLES: dict[str, str] = dotenv_values('../.env')
+ENVIRONMENT_VARIABLES: dict[str, str] = {**dotenv_values('../.env'), **os.environ}
 
 BASE_URL: str = "https://finki.ukim.mk"
 STUDY_PROGRAMS_URL: str = "https://finki.ukim.mk/mk/dodiplomski-studii"
 THREADS_PER_CPU_CORE: int = 5
-COURSE_CODE_PATTERN: str = r"F23L[1-3][SW]\d{3}"
-OUTPUT_DIRECTORY_PATH: Path = Path(ENVIRONMENT_VARIABLES.get('OUTPUT_DIRECTORY_PATH'))
 MAX_WORKERS: int = THREADS_PER_CPU_CORE * os.cpu_count() if ENVIRONMENT_VARIABLES.get('MAX_WORKERS') == 'MAX_WORKERS' else (
     int(ENVIRONMENT_VARIABLES.get('MAX_WORKERS')))
 EXECUTOR_TYPE: str = ENVIRONMENT_VARIABLES.get('EXECUTOR_TYPE')
