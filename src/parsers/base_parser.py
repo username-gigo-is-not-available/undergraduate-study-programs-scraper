7c63b21264b4956af9438fe36b7593dbd1e564b5
unknown <grigorij.josifovski@gmail.com>
2025-02-19 21:33:49 +0100

    Move base and field parser

--- src/parsers/base_parser.py
+++ src/parsers/base_parser.py
@@ -1,7 +1,8 @@
 from src.patterns.mixin.data_processing import ProcessingMixin
 from src.patterns.mixin.file_storage import FileStorageMixin
 from src.patterns.mixin.http_client import HTTPClientMixin
+from src.patterns.mixin.thread_safety import ThreadSafetyMixin
 
 
-class Parser(ProcessingMixin, FileStorageMixin, HTTPClientMixin):
+class Parser(ProcessingMixin, FileStorageMixin, HTTPClientMixin, ThreadSafetyMixin):
     pass
