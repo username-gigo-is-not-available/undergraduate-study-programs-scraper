e688274896cbc7fdb31045729ecb4e8ed5878a57
grigo <grigorij.josifovski@gmail.com>
2025-04-16 09:44:08 +0200

    Update MinioClient

--- src/clients.py
+++ src/clients.py
@@ -4,7 +4,11 @@ from src.config import Config
 
 
 class MinioClient:
+    MINIO_ENDPOINT_URL: str = Config.MINIO_ENDPOINT_URL
+    MINIO_ACCESS_KEY: str = Config.MINIO_ACCESS_KEY
+    MINIO_SECRET_KEY: str = Config.MINIO_SECRET_KEY
 
     @staticmethod
     def connect():
-        return Minio(Config.MINIO_ENDPOINT_URL, access_key=Config.MINIO_ACCESS_KEY, secret_key=Config.MINIO_SECRET_KEY, secure=False)
+        return Minio(endpoint=MinioClient.MINIO_ENDPOINT_URL, access_key=MinioClient.MINIO_ACCESS_KEY,
+                     secret_key=MinioClient.MINIO_SECRET_KEY, secure=False)
