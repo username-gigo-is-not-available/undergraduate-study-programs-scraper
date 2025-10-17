248b12dd32ead0fb75c00a46705768f6f111f850
grigo <grigorij.josifovski@gmail.com>
2025-10-18 01:57:20 +0200

    Add IcebergClient, rename files

--- src/network.py
+++ src/network.py
@@ -1,30 +1,29 @@
 import asyncio
 import logging
 from ssl import SSLContext
-from typing import Any, Coroutine, NamedTuple
+from typing import NamedTuple
 
-import aiohttp
-from aiohttp import ClientTimeout, ClientError
+from aiohttp import ClientError, ClientTimeout, ClientSession
+
 from tenacity import retry, wait_fixed, retry_if_exception_type, stop_after_attempt
-from src.configurations import  ApplicationConfiguration
+
+from src.configurations import ApplicationConfiguration
 
 
-class HTTPClientMixin:
+class HTTPClient:
 
-    @classmethod
     @retry(
         stop=stop_after_attempt(ApplicationConfiguration.REQUEST_RETRY_COUNT),
         wait=wait_fixed(ApplicationConfiguration.REQUESTS_RETRY_DELAY_SECONDS),
         retry=retry_if_exception_type((asyncio.TimeoutError, ClientError)),
         reraise=True
     )
-    async def fetch_page(cls, session: aiohttp.ClientSession, ssl_context: SSLContext, url: str) -> tuple[int, str]:
+    async def fetch_page(self, session: ClientSession, ssl_context: SSLContext, url: str) -> tuple[int, str]:
         async with session.get(url, ssl=ssl_context, timeout=ClientTimeout(total=ApplicationConfiguration.REQUESTS_TIMEOUT_SECONDS)) as response:
                 logging.info(f"Fetching page {url}")
                 return response.status, await response.text()
 
-    @classmethod
-    async def fetch_page_wrapper(cls, session: aiohttp.ClientSession, ssl_context: SSLContext, url: str,
+    async def fetch_page_wrapper(self, session: ClientSession, ssl_context: SSLContext, url: str,
                                  named_tuple: NamedTuple) -> tuple[int, str, NamedTuple]:
-        http_status, page_content = await cls.fetch_page(session, ssl_context, url)
+        http_status, page_content = await self.fetch_page(session, ssl_context, url)
         return http_status, page_content, named_tuple
---
+++ src/storage.py
@@ -0,0 +1,64 @@
+import logging
+from typing import NamedTuple, Any
+
+from pyiceberg.catalog import load_catalog, Catalog
+from miniopy_async import Minio
+from pyiceberg.table import Table
+
+from src.configurations import StorageConfiguration, DatasetConfiguration
+import pyarrow as pa
+
+from src.models.enums import FileIOType
+
+
+class IcebergClient:
+    _instance: 'IcebergClient' = None
+    _s3_client: Minio = None
+    _catalog: Catalog = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if self._catalog is not None:
+            return
+        logging.info("Initializing IcebergClient resources...")
+        self._catalog = load_catalog(
+            StorageConfiguration.ICEBERG_CATALOG_NAME
+        )
+        if self._s3_client is None and StorageConfiguration.FILE_IO_TYPE == FileIOType.S3:
+            self._s3_client = Minio(
+                endpoint=StorageConfiguration.S3_ENDPOINT_URL,
+                access_key=StorageConfiguration.S3_ACCESS_KEY,
+                secret_key=StorageConfiguration.S3_SECRET_KEY,
+                secure=False
+            )
+
+    def get_catalog(self) -> Catalog:
+        return self._catalog
+
+    def get_s3_client(self) -> Minio | None:
+        return self._s3_client
+
+    @classmethod
+    def get_table_identifier(cls,  namespace: str, table_name: str) -> str:
+        return f"{namespace}.{table_name}"
+
+    async def save_data(self, data: list[NamedTuple], dataset_config: DatasetConfiguration) -> list[dict[str, Any]]:
+        catalog: Catalog = self.get_catalog()
+
+        data: list[dict[str, Any]] = [row._asdict() for row in data]
+        table_identifier: str = self.get_table_identifier(StorageConfiguration.ICEBERG_NAMESPACE, dataset_config.dataset_name)
+        table: Table = catalog.load_table(table_identifier)
+
+        logging.info(f"Saving data to {table_identifier} with schema {dataset_config.schema} and {len(data)} rows")
+
+        pa_table = pa.Table.from_pylist(mapping=data, schema=dataset_config.schema.as_arrow())
+        table.append(pa_table)
+
+        logging.info(f"Created snapshot_id: {table.current_snapshot().snapshot_id} for table {table_identifier}")
+        return data
+
+
