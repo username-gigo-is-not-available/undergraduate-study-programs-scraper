49e08ed6bc6934d3f5c8337d2b4714cf3e78678a
grigo <grigorij.josifovski@gmail.com>
2025-10-26 16:58:00 +0100

    Remove prefixes and en columns

--- src/corrector.py
+++ src/corrector.py
@@ -7,13 +7,12 @@ class CourseCorrector:
 
     @staticmethod
     def correct(fields: dict[str, str]) -> dict[str, str | None]:
-        course_name = fields.get('course_name_mk') or fields.get('course_name_en')
+        course_name = fields.get('name')
 
         if course_name and re.search(ApplicationConfiguration.COURSE_CODES_REGEX, course_name):
-            course_name_key = 'course_name_mk' if 'course_name_mk' in fields else 'course_name_en'
             fields.update({
-                'course_code': CourseCorrector.extract_course_code(course_name),
-                course_name_key: CourseCorrector.extract_course_name(course_name)
+                'code': CourseCorrector.extract_course_code(course_name),
+                'name': CourseCorrector.extract_course_name(course_name)
             })
 
         return fields
--- src/schemas/course_schema.py
+++ src/schemas/course_schema.py
@@ -4,56 +4,49 @@ from pyiceberg.types import StringType, NestedField
 COURSE_SCHEMA = Schema(
     NestedField(
         id=1,
-        name="course_code",
+        name="code",
         field_type=StringType(),
         required=True,
         doc="The unique identifier code for the course (pattern: ^F23L[1-3][SW]\\d{3})."
     ),
     NestedField(
         id=2,
-        name="course_name_mk",
+        name="name",
         field_type=StringType(),
         required=True,
-        doc="The official name of the course in Macedonian."
+        doc="The official name of the course."
     ),
     NestedField(
         id=3,
-        name="course_name_en",
-        field_type=StringType(),
-        required=True,
-        doc="The official name of the course in English."
-    ),
-    NestedField(
-        id=4,
-        name="course_url",
+        name="url",
         field_type=StringType(),
         required=True,
         doc="The unique URL to the official course description or page."
     ),
     NestedField(
-        id=5,
-        name="course_professors",
+        id=4,
+        name="professors",
         field_type=StringType(),
         required=False,
         doc="A comma-separated string of the main professors teaching this course, if available."
     ),
     NestedField(
-        id=6,
-        name="course_prerequisites",
+        id=5,
+        name="prerequisites",
         field_type=StringType(),
         required=False,
         doc="A text description of the prerequisites required to enroll in this course. Can be null if no prerequisites are listed."
     ),
     NestedField(
-        id=7,
-        name="course_competence",
+        id=6,
+        name="competence",
         field_type=StringType(),
         required=True,
         doc="A text description of the learning outcomes or competencies students will gain upon completing the course."
     ),
     NestedField(
-        id=8,
-        name="course_content",
+        id=7,
+        name="content",
         field_type=StringType(),
         required=True,
         doc="A text description of the course's lectures or study plan."
