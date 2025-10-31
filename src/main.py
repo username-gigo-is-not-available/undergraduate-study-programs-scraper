79260aade1923592e5c361567f648ad609487e2e
grigo <grigorij.josifovski@gmail.com>
2025-10-31 20:32:00 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ from src.network import HTTPClient
 from src.parsers.accreditation_2023.course_parser import CourseParser
 from src.parsers.accreditation_2023.curriculum_parser import CurriculumParser
 from src.parsers.accreditation_2023.study_program_parser import StudyProgramParser
+# from src.parsers.accreditation_2018.study_program_parser import StudyProgramParser
 from src.storage import IcebergClient
 
 logging.basicConfig(level=logging.INFO, force=True)
@@ -26,10 +27,12 @@ async def main():
     ssl_context: ssl.SSLContext = ssl.create_default_context(cafile=certifi.where())
     http_client: HTTPClient = HTTPClient()
     iceberg_client: IcebergClient = IcebergClient()
+    semaphore: asyncio.Semaphore = asyncio.Semaphore(ApplicationConfiguration.NUMBER_OF_CONCURRENT_REQUESTS)
     tasks: list[asyncio.Task] = [asyncio.create_task(StudyProgramParser().run(session=session,
                                                                               ssl_context=ssl_context,
                                                                               iceberg_configuration=STUDY_PROGRAMS,
                                                                               http_client=http_client,
+                                                                              semaphore=semaphore,
                                                                               iceberg_client=iceberg_client))]
     with ThreadPoolExecutor(max_workers=ApplicationConfiguration.NUMBER_OF_THREADS) as executor:
         tasks.append(asyncio.create_task(CurriculumParser().run(session=session,
@@ -44,6 +47,7 @@ async def main():
                                                             executor=executor,
                                                             iceberg_configuration=COURSES,
                                                             http_client=http_client,
+                                                            semaphore=semaphore,
                                                             iceberg_client=iceberg_client)))
         await asyncio.gather(*tasks)
     await session.close()
