bfe09062013f2ca6328338f203940c4b3bd60119
grigo <grigorij.josifovski@gmail.com>
2025-11-20 13:28:22 +0100

    Remove unused methods

--- src/models/enums.py
+++ src/models/enums.py
@@ -11,26 +11,8 @@ class OfferingType(UpperStrEnum):
     ELECTIVE = auto()
 
     @classmethod
-    def from_bool(cls, value: bool) -> 'OfferingType':
-        return cls.ELECTIVE if value else cls.MANDATORY
-
-    @classmethod
-    def from_string(cls, value: str) -> 'OfferingType':
-        if 'Задолжителни предмети'.casefold() == value.casefold():
-            return cls.MANDATORY
-        elif 'Изборни предмети'.casefold() == value.casefold():
-            return cls.ELECTIVE
-        else:
-            raise ValueError(value)
-
-
-class SemesterSeasonType(UpperStrEnum):
-    WINTER = auto()
-    SUMMER = auto()
-
-    @classmethod
-    def from_str(cls, value: str) -> 'SemesterSeasonType':
-        return cls.WINTER if value.casefold() == 'Зимски'.casefold() else cls.SUMMER
+    def from_parity(cls, value: int) -> 'OfferingType':
+        return cls.MANDATORY if value % 2 else cls.ELECTIVE
 
 class FileIOType(UpperStrEnum):
     S3 = auto()
